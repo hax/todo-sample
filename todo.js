0d024d6cc61b831f44c4e99a9fd11f743fbbc1b4
HE Shi-Jun <hax@heshijun.net>
2014-11-03 18:51:13 +0800

    broadcast

--- todo.js
+++ todo.js
@@ -1,40 +1,49 @@
 'use strict'
 
 function TodoList() {
-	var _this = this
-	window.addEventListener('storage', function (e) {
-		if (e.key === 'todo-list') {
-			_this.load()
-			if (typeof _this.onupdate === 'function') _this.onupdate()
-		}
-	})
 }
 TodoList.prototype.load = function () {
-	var data = localStorage.getItem('todo-list')
-	if (data) data = JSON.parse(data)
-	else data = [{content: 'Task 1', done: true}, {content: 'Task 2', done: false}]
-	this.data = data
+	this.data = [{content: 'Task 1', done: true}, {content: 'Task 2', done: false}]
 }
 TodoList.prototype.save = function () {
-	localStorage.setItem('todo-list', JSON.stringify(this.data))
 }
 TodoList.prototype.add = function (content) {
 	this.data.push({content: content, done: false})
 	this.save()
 }
-TodoList.prototype.updateStatus = function (index, done) {
+TodoList.prototype.update = function (index, done) {
 	this.data[index].done = done
 	this.save()
 }
 
-//var todo = new TodoList
+var todo = new TodoList
+todo.load()
 
+var allPorts = []
 onconnect = function (e) {
 	var port = e.ports[0]
-	port.postMessage('connected')
+	allPorts.push(port)
+	port.postMessage('Shared worker connected ' + e.ports.length + ', ' + allPorts.length)
+	port.postMessage({command: 'load', list: todo.data})
 	port.onmessage = function (e) {
-		console.log(e.data)
+		port.postMessage('recieved command: ' + e.data.command)
+		switch (e.data.command) {
+			case 'add':
+				todo.add(e.data.content)
+				broadcast('add', todo.data[todo.data.length - 1], todo.data.length - 1)
+				break
+			case 'update':
+				todo.update(e.data.index, e.data.done)
+				broadcast('update', todo.data[e.data.index], e.data.index)
+				break
+			default:
+				port.postMessage('Unknown command: ' + e.data.command)
+		}
 	}
 }
 
-
+function broadcast(command, item, index) {
+	allPorts.forEach(function (port) {
+		port.postMessage({command: command, item: item, index: index})
+	})
+}
